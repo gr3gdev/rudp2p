0f97eb8956befb362075dce2c3cec55c28e0987a
Gregory Tardivel <gregory.tardivel@kosmos.fr>
2023-10-31 10:23:49 +0100

    separate wasm module

--- build_locally.sh
+++ build_locally.sh
@@ -6,16 +6,15 @@ docker-compose -f rudp2p/tests/compose.yml up -d
 # Build
 cargo build --verbose
 
-# Compatibility WASM
-rustup target add wasm32-unknown-unknown
-cargo build --lib --target wasm32-unknown-unknown
-
-cd rudp2p
 # Run tests with default features
 cargo test
 # Run tests with feature ""ssl"
 cargo test --features="ssl"
-cd ..
 
 # Clear tests
 docker-compose -f rudp2p/tests/compose.yml down
+
+# Compatibility WASM
+cd wasm
+rustup target add wasm32-unknown-unknown
+cargo build --lib --target wasm32-unknown-unknown
